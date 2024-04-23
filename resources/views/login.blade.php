8ef25c2ada53b6b795e30129de322085898e9189
lalaggv2 <luangu2020@gmail.com>
2024-04-23 10:38:04 -0500

    centrando el login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -108,7 +108,7 @@
     <link href="{{ asset('assets/css/login.css') }}" rel="stylesheet">
   </head>
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
-    <div class='parent_image1'>
+    {{-- <div class='parent_image1'> --}}
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
       <symbol id="check2" viewBox="0 0 16 16">
         <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
@@ -189,6 +189,6 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-</div>
+{{-- </div> --}}
     </body>
 </html>
